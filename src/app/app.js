257410df2bf2a3adfb533486d9bf1a6c377d66ae
Adam Klein <adam@500tech.com>
2015-07-16 15:23:44 +0300

    Initial version

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+import 'angular';
+import '../../node_modules/angular-ui-grid/ui-grid';
+
+import appStyle    from 'app/app.less';
+import appTemplate from 'app/app.html';
+import Main 	   from './main';
+import MyGrid 	   from './my_grid';
+
+export default angular.module('app', ['ui.grid'])
+  .controller('Main', Main)
+  .service('MyGrid', MyGrid)
+
---
+++ src/app/main.js
@@ -0,0 +1,9 @@
+import myData from './data.json'
+export default class Main {
+	constructor(MyGrid) {
+		this.gridOptions = {
+			data: myData
+		};
+	}
+}
+
