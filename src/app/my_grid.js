4250bcbcb9455a7f75da01b7d25a6158ec36ace4
Adam Klein <adam@500tech.com>
2015-07-16 15:29:50 +0300

    defaults

--- src/app/my_grid.js
+++ src/app/my_grid.js
@@ -1,4 +1,17 @@
+const DEFAULTS = {
+	enableFiltering: true
+};
+
 class MyGrid {
+	constructor(opts) {
+		this._setDefaults(opts);
+	}
+
+	_setDefaults(opts) {
+		opts = angular.extend(DEFAULTS, opts);
+		angular.extend(this, opts)
+	}
+
 	setData(data) {
 		this.data = data;
 		return this;
